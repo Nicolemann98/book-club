083f6e577615dfe61bed8434e496435126ec1f82
Nicole Mann <nicolemann98@outlook.com>
2024-10-02 22:14:44 +0000

    deployment steps

--- products/models.py
+++ products/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.URLField(max_length=1024, null=True, blank=True)
     img_paths = models.ImageField(null=True, blank=True)
-    format = models.CharField(max_length=16, null=True, blank=True)
+    format = models.CharField(max_length=254, null=True, blank=True)
     currency = models.CharField(max_length=2, null=True, blank=True)
 
     def __str__(self):
