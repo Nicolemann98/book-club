1666036890d20d77c81e622b52b83b0b6d007afc
Nicole Mann <nicolemann98@outlook.com>
2024-11-15 16:27:24 +0000

    Adding simple models

---
+++ newsletter/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class NewsletterSubscription(models.Model):
+    email = models.EmailField(unique=True)
+    date_subscribed = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+class Newsletter(models.Model):
+    date_sent = models.DateTimeField(auto_now_add=True)
+    # file storage location
+
+
+class NewsletterSent(models.Model):
+    newsletter = models.ForeignKey(
+        Newsletter,
+        on_delete=models.CASCADE
+    )
+    newsletter_subscription = models.ForeignKey(
+        NewsletterSubscription,
+        on_delete=models.CASCADE
+    )
