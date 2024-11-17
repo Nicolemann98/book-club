f4896c384c8467bcc50e762baecd6f083126833d
Nicole Mann <nicolemann98@outlook.com>
2024-11-17 17:25:35 +0000

    fix sign up

--- book_club/settings.py
+++ book_club/settings.py
@@ -218,19 +218,14 @@ STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
-# Email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
-print("EMAIL_BACKEND = " + EMAIL_BACKEND)
-print("EMAIL_USE_TLS = " + str(EMAIL_USE_TLS))
-print("EMAIL_PORT = " + str(EMAIL_PORT))
-print("EMAIL_HOST = " + EMAIL_HOST)
-print("EMAIL_HOST_USER = " + EMAIL_HOST_USER)
-print("EMAIL_HOST_PASSWORD = " + EMAIL_HOST_PASSWORD)
-print("DEFAULT_FROM_EMAIL = " + DEFAULT_FROM_EMAIL)
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'cookclub@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
