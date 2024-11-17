5d51e07bdc5788e9186c8eebdf2802ef365a7997
Nicole Mann <nicolemann98@outlook.com>
2024-11-17 15:42:46 +0000

    Add subscribe links to main page

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('manage/', views.subscribe_newsletter, name='newsletter_management'),
     path('subscribe/', views.subscribe_newsletter, name='subscribe_newsletter'),
     path('unsubscribe/', views.unsubscribe_newsletter, name='unsubscribe_newsletter'),
 ]
