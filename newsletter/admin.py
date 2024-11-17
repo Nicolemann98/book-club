1a7f0548f84c4d871b56869a86b1af340e05b183
Nicole Mann <nicolemann98@outlook.com>
2024-11-17 12:41:06 +0000

    migrations

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import NewsletterSubscription#, Newsletter, NewsletterSent
+from .models import NewsletterSubscription, Newsletter, NewsletterSent
 
 # Register your models here.
 
@@ -8,23 +8,22 @@ class NewsletterSubscriptionAdmin(admin.ModelAdmin):
     list_display = (
         'email',
         'date_subscribed',
-        'is_active',
     )
 
 
-# class NewsletterAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'date_sent',
-#     )
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = (
+        'date_sent',
+    )
 
 
-# class NewsletterSentAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'newsletter',
-#         'newsletter_subscription',
-#     )
+class NewsletterSentAdmin(admin.ModelAdmin):
+    list_display = (
+        'newsletter',
+        'newsletter_subscription',
+    )
 
 
 admin.site.register(NewsletterSubscription, NewsletterSubscriptionAdmin)
-# admin.site.register(Newsletter, NewsletterAdmin)
-# admin.site.register(NewsletterSent, NewsletterSentAdmin)
+admin.site.register(Newsletter, NewsletterAdmin)
+admin.site.register(NewsletterSent, NewsletterSentAdmin)
